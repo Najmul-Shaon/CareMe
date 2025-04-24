13e26d2fd08a087d29d32545a3ea445814710b2c
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-04-24 13:56:30 +0600

    stat section added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layouts/layout";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
