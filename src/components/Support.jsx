1ec384162ce2022287f0b95ca1306acff8a66bf1
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-04-26 13:14:49 +0600

    added support section

---
+++ src/components/Support.jsx
@@ -0,0 +1,13 @@
+const Support = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="text-4xl font-extrabold text-primaryColor max-w-xl leading-11">
+          Exclusive Supplier+ Rewards for the first 30 days
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
