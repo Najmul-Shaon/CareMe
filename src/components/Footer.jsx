c87edb60b06f5081a4b9a0c7b5fe605534ab4f30
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-04-26 15:27:44 +0600

    designed foooter section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,60 +1,65 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10 font-open-sans">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Social</h6>
-        <div className="grid grid-flow-col gap-4">
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </a>
+    <footer className="bg-primaryColor mt-18 text-footerTextColor">
+      <div className="max-w-screen-xl mx-auto py-12 px-4 md:px-0">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-56 gap-y-12">
+          {/* site info  */}
+          <div className="space-y-5">
+            <h1 className="text-4xl font-extrabold">Care Me</h1>
+            <p className="text-lg md:max-w-58">
+              Sell your products to crores of customers on Meesho at 0%
+              commission
+            </p>
+            <button className="btn btn-sm px-4 py-2 bg-accentColor border-0 text-white text-lg">
+              Start Selling
+            </button>
+          </div>
+          {/* details about care me */}
+          <div className="space-y-5">
+            <h3 className="font-bold text-xl">Sell on Care Me</h3>
+
+            <ul className="space-y-3 text-lg">
+              <li className="hover:underline cursor-pointer">Sell Online</li>
+              <li className="hover:underline cursor-pointer">
+                Pricing & Commission
+              </li>
+              <li className="hover:underline cursor-pointer">How it works</li>
+              <li className="hover:underline cursor-pointer">
+                Shipping & Returns
+              </li>
+              <li className="hover:underline cursor-pointer">
+                Grow Your Business
+              </li>
+              <li className="hover:underline cursor-pointer">Learning Hub</li>
+            </ul>
+          </div>
+
+          {/* support  */}
+          <div className="space-y-5">
+            <h3 className="font-bold text-xl">Sell on Care Me</h3>
+            <p className="text-lg">support@caremebd.com</p>
+            <div className="flex items-center gap-1">
+              <p className="bg-backgroundColor text-primaryColor w-8 h-8 flex items-center justify-center text-2xl font-bold rounded-lg">
+                F
+              </p>
+              <p className="bg-backgroundColor text-primaryColor w-8 h-8 flex items-center justify-center text-2xl font-bold rounded-lg">
+                I
+              </p>
+              <p className="bg-backgroundColor text-primaryColor w-8 h-8 flex items-center justify-center text-2xl font-bold rounded-lg">
+                Y
+              </p>
+            </div>
+          </div>
         </div>
-      </nav>
+        <div className="h-[1px] bg-backgroundColor my-12"></div>
+        <div className="footer sm:footer-horizontal footer-center">
+          <aside>
+            <p className="text-xl">
+              © {new Date().getFullYear()} Care Me. All Rights Reserved.
+            </p>
+          </aside>
+        </div>
+      </div>
     </footer>
   );
 };
